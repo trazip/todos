416a6608b7bfbecc8527bb0cea33f8d9534347c5
Pierre Penhard <72160319+trazip@users.noreply.github.com>
2021-12-14 16:15:24 +0100

    User can sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def authenticate
+    unless signed_in?
+      redirect_to new_session_path
+    end
+  end
+
+  def signed_in?
+    session[:current_email].present?
+  end
 end
---
+++ spec/support/features/sign_in.rb
@@ -0,0 +1,10 @@
+module Features
+  def sign_in
+    visit root_path
+
+    fill_in 'Email', with: 'person@example.com'
+    click_on 'Sign in'
+  end
+end
+
+RSpec.configure { |config| config.include Features }
